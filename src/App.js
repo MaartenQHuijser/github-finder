1817dadcff6a6c04748ce67857bd6c5a44a865d7
Maarten Huijser <maarten@huijser.nl>
2021-06-29 13:58:29 +0200

    added: single user

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar';
+import UserItem from './components/users/UserItem';
 
 class App extends Component {
-  foo = () => 'Bars';
-
   render() {
     return (
       <div className='App'>
         <Navbar />
+        <UserItem />
       </div>
     );
   }
